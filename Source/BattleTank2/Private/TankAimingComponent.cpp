cbe2147275f1112d12ced7428cabc160d359de09
SM-SB\smait <s.maitland@me.com>
2016-09-06 18:07:53 +0100

    Predict aiming at

--- Source/BattleTank2/Private/TankAimingComponent.cpp
+++ Source/BattleTank2/Private/TankAimingComponent.cpp
@@ -34,7 +34,25 @@ void UTankAimingComponent::TickComponent( float DeltaTime, ELevelTick TickType, 
 }
 
 void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchgSpeed){
-	UE_LOG(LogTemp, Warning, TEXT("Firing at %F"), LaunchgSpeed);
+	if (!Barrel) { return; }
+	FVector OutLaunchVelocity;
+	FVector StartLocation = Barrel->GetSocketLocation(FName("Projectile"));
+
+	// Calculate the OutLaunchVelocity
+	if (UGameplayStatics::SuggestProjectileVelocity(
+			this,
+			OutLaunchVelocity,
+			StartLocation,
+			HitLocation,
+			LaunchgSpeed,
+			false,
+			0,
+			0,
+			ESuggestProjVelocityTraceOption::DoNotTrace)){
+		auto AimDirection = OutLaunchVelocity.GetSafeNormal();
+		auto TankName = GetOwner()->GetName();
+		UE_LOG(LogTemp, Warning, TEXT("%s aiming at %s"), *TankName, *AimDirection.ToString())
+	}
 }
 
 void UTankAimingComponent::SetBarrelReference(UStaticMeshComponent* BarrelToSet){
