58902de1ace93724a66746c15d3a8cbf91c51ae9
SM-SB\smait <s.maitland@me.com>
2016-09-13 19:10:26 +0100

    Throttle moves tank (in the air!)

--- Source/BattleTank2/Private/TankTrack.cpp
+++ Source/BattleTank2/Private/TankTrack.cpp
@@ -9,4 +9,10 @@ void UTankTrack::SetThrottle(float Throttle){
 	auto Name = GetName();
 	UE_LOG(LogTemp, Warning, TEXT("%s throttle: %f"), *Name, Throttle)
 
+	// TODO clamp actual throttle value so player can't override
+
+	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
+	auto ForceLocation = GetComponentLocation();
+	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
 }
--- Source/BattleTank2/Public/TankTrack.h
+++ Source/BattleTank2/Public/TankTrack.h
@@ -18,5 +18,7 @@ public:
 	UFUNCTION(BlueprintCallable, Category = Input)
 	void SetThrottle(float Throttle);
 	
-	
+	// This is max force per track in Newtons
+	UPROPERTY(EditDefaultsOnly)
+		float TrackMaxDrivingForce = 400000.0;	// Assume 40 tonne tank and 1g acceleration 
 };
