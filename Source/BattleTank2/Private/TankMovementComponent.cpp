1c0756ede0142d7540403edfb4c61efbcbc284f4
SM-SB\smait <s.maitland@me.com>
2016-09-14 06:49:27 +0100

    Tank Movement Component Suppress AI Aiming Straight to Game (Event Map Start - non SC)

---
+++ Source/BattleTank2/Private/TankMovementComponent.cpp
@@ -0,0 +1,8 @@
+// It's mine. Leave it alone
+
+#include "BattleTank2.h"
+#include "TankMovementComponent.h"
+
+
+
+
---
+++ Source/BattleTank2/Public/TankMovementComponent.h
@@ -0,0 +1,19 @@
+// It's mine. Leave it alone
+
+#pragma once
+
+#include "GameFramework/NavMovementComponent.h"
+#include "TankMovementComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK2_API UTankMovementComponent : public UNavMovementComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
